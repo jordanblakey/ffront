129ad02fe5745822eeda21f1ea9638dd09272171
Jordan Blakey <jordan@jordanblakey.com>
2017-12-18 07:47:47 -0600

    added better image compression examples. Also added docs folder for Github Pages.

--- src/assets/js/_tests/app.test.js
+++ src/assets/js/_tests/app.test.js
@@ -7,7 +7,6 @@ const sum = require('../app').sum
 //   return a + b
 // }
 
-
 test('adds 1 + 2 to equal 3', () => {
   expect(sum(1, 2)).toBe(3)
 })
--- src/assets/js/_tests/jasmine-check.test.js
+++ src/assets/js/_tests/jasmine-check.test.js
@@ -10,16 +10,28 @@ function sum(a, b) {
 }
 
 describe('Generative property testing examples with jasmine-check', () => {
-  check.it('addition is commutative', {times:10}, gen.int, gen.int, (x, y) => {
-    expect(sum(x, y)).toBe(y + x)
-  })
+  check.it(
+    'addition is commutative',
+    { times: 10 },
+    gen.int,
+    gen.int,
+    (x, y) => {
+      expect(sum(x, y)).toBe(y + x)
+    }
+  )
 
-  check.it('accepts an int and a string', {times:10}, gen.int, gen.string, (x, y) => {
-    expect(x).toEqual(expect.any(Number));
-    expect(y).toEqual(expect.any(String));
-  })
+  check.it(
+    'accepts an int and a string',
+    { times: 10 },
+    gen.int,
+    gen.string,
+    (x, y) => {
+      expect(x).toEqual(expect.any(Number))
+      expect(y).toEqual(expect.any(String))
+    }
+  )
 
-  check.it('runs 10 times', {times: 10}, gen.sPosInt, x => {
-    expect(x).toBeGreaterThan(0);
+  check.it('runs 10 times', { times: 10 }, gen.sPosInt, x => {
+    expect(x).toBeGreaterThan(0)
   })
 })
