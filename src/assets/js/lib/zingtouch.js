a20a8b26662334181944c5b3b425e40f0afa2c0e
Jordan Blakey <jordan@jordanblakey.com>
2017-12-19 14:10:47 -0600

    completed Rellax and Zingtouch example pages

--- src/assets/js/lib/zingtouch.js
+++ src/assets/js/lib/zingtouch.js
@@ -8,12 +8,30 @@ const elem = document.getElementById("touch-target")
 
 if (elem !== null) {
   // Bind Built-in Gestures to DOM target
-  zt.bind(elem, "tap", function(e) {elem.children[0].textContent = "TAP event deteacted on: " + elem.id; }, false)
-  zt.bind(elem, "swipe", function(e) {elem.children[0].textContent = "SWIPE event deteacted on: " + elem.id; }, true)
-  zt.bind(elem, "pan", function(e) {elem.children[0].textContent = "PAN event deteacted on: " + elem.id; }, true)
-  zt.bind(elem, "pinch", function(e) {elem.children[0].textContent = "PINCH event deteacted on: " + elem.id; }, false)
-  zt.bind(elem, "expand", function(e) {elem.children[0].textContent = "EXPAND event deteacted on: " + elem.id; }, false)
-  zt.bind(elem, "rotate", function(e) {elem.children[0].textContent = "ROTATE event deteacted on: " + elem.id; }, false)
+  zt.bind(elem, "tap", function(e) {
+    elem.children[6].textContent = "TAP event deteacted on: " + elem.id;
+    elem.children[0].style.opacity = 1;
+  }, false)
+  zt.bind(elem, "swipe", function(e) {
+    elem.children[6].textContent = "SWIPE event deteacted on: " + elem.id;
+    elem.children[1].style.opacity = 1;
+  }, true)
+  zt.bind(elem, "pan", function(e) {
+    elem.children[6].textContent = "PAN event deteacted on: " + elem.id;
+    elem.children[2].style.opacity = 1;
+  }, true)
+  zt.bind(elem, "pinch", function(e) {
+    elem.children[6].textContent = "PINCH event deteacted on: " + elem.id;
+    elem.children[4].style.opacity = 1;
+  }, false)
+  zt.bind(elem, "expand", function(e) {
+    elem.children[6].textContent = "EXPAND event deteacted on: " + elem.id;
+    elem.children[5].style.opacity = 1;
+  }, false)
+  zt.bind(elem, "rotate", function(e) {
+    elem.children[6].textContent = "ROTATE event deteacted on: " + elem.id;
+    elem.children[3].style.opacity = 1;
+  }, false)
 
   // Create a custom gesture
   const shorttap = new ZingTouch.Tap({ maxDelay: 100 })
